60d62c4da461f60858d3c671f3abfabbb90770c7
Vicky Cas <jeanvictoriafloreal@gmail.com>
2021-08-31 20:27:16 -0300

    Finiquito

---
+++ javascript/menu.js
@@ -0,0 +1,14 @@
+// var iconMenu = document.getElementById ("icon-menu");
+// var mainMenu = document.getElementById ("main-menu");
+
+// iconMenu.addEventListener("click",()=>mainMenu.classList.toggle("menu--show"));
+
+// var iconMenu=document.getElementById("icon-menu"),mainMenu=document.getElementById("main-menu");
+// iconMenu.addEventListener("click",function(){mainMenu.classList.toggle("nav--show")});
+
+
+
+const iconMenu = document.getElementById('icon-menu');
+const mainMenu = document.getElementById('main-menu');
+
+iconMenu.addEventListener('click', ()=>mainMenu.classList.toggle('menu--show'));
